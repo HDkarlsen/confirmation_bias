59b985564cf0d095e8fd1ddf5240e39344e512a8
hkarlsen <h.karlsen@ntnu.no>
2023-05-12 13:36:18 +0200

    Added function that runs checks and then the rule.

--- ruleOfThree.R
+++ ruleOfThree.R
@@ -9,17 +9,25 @@ wrong3 <- c(1, 2, "3")
 
 tests <- list(right1, right2, wrong1, wrong2, wrong3)
 
-# A functiont that evaluates the attempt
-
+# A function that evaluates the attempt and returns TRUE/FALSE.
 test_rule <- function(attempt) {
-  
-  if (length(attempt) != 3) return("Error: Wrong length. Try again.")
-  
-  if (!is.numeric(attempt)) return("Error: I only accept numbers. Try again")
-  
   result <- attempt[1] < attempt[2] & attempt[2] < attempt[3]
-  
   result
 }
 
-test_rule(wrong3)
+# Checks for errors. If not, returns same vector.
+catch_error <- function(attempt) {
+  if (length(attempt) != 3) return("Error: Wrong length. Try again.")
+  if (!is.numeric(attempt)) return("Error: I only accept numbers. Try again.")
+  return(attempt)
+}
+
+# Checks for errors and in the absence it returns the result of the test.
+run_test <- function(attempt) {
+  checked_attempt <- catch_error(attempt)
+  
+  if (is.character(checked_attempt)) return(checked_attempt)
+  
+  test_rule(attempt)
+}
+
