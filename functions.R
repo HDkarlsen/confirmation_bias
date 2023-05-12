421f82208edb08c350913c8ddc9d0a9b9d6e26e2
hkarlsen <h.karlsen@ntnu.no>
2023-05-12 15:04:33 +0200

    Incorporated clean_input into run_test and fixed error searching.

--- functions.R
+++ functions.R
@@ -2,28 +2,31 @@
 
 library(magrittr)
 
-# Test test
-right1 <- c(2, 4, 6)
-right2 <- c(-2, 6, 110)
-wrong1 <- c(1, 2, 3, 4)
-wrong2 <- c(1, 2, 0)
-wrong3 <- c(1, 2, "3")
-
-tests <- list(right1, right2, wrong1, wrong2, wrong3)
-
-clean_test <- "2, 4, 6"
 # Takes a list of three numbers and cleans it.
 clean_input <- function(numbers) {
   # Assumes three digits in a string, separated by a comma
-  # Removes whitespace, then splits on comma, turns into numeric
+  # Removes whitespace, splits on comma, turns into numeric
   out <- numbers %>% 
     stringr::str_remove_all(" ") %>% 
-    stringr::str_split_1(",") %>% 
-    as.numeric()
+    stringr::str_split_1(",") 
+  out <- tryCatch(
+    {
+      # If no error/warning spotted
+      as.numeric(out)
+    },
+    error = function(cond) {
+      message("Seems like you entered something wrong. Read the instructions and try again")
+      return("")
+    },
+    warning = function(cond) {
+      message("Seems you entered something wrong. Read the instructions and try again.")
+      return("")
+    }
+  )
   out
 }
 
-# A function that evaluates the attempt and returns TRUE/FALSE.
+# Evaluates the attempt and returns TRUE/FALSE.
 test_rule <- function(attempt) {
   result <- attempt[1] < attempt[2] & attempt[2] < attempt[3]
   result
@@ -31,17 +34,16 @@ test_rule <- function(attempt) {
 
 # Checks for errors. If not, returns same vector.
 catch_error <- function(attempt) {
-  if (length(attempt) != 3) return("Error: Wrong length. Try again.")
   if (!is.numeric(attempt)) return("Error: I only accept numbers. Try again.")
+  if (length(attempt) != 3) return("Error: Wrong length. Try again.")
   return(attempt)
 }
 
-# Checks for errors and in the absence it returns the result of the test.
+# Combines the three functions into one.
 run_test <- function(attempt) {
+  attempt <- clean_input(attempt) 
   checked_attempt <- catch_error(attempt)
-  
   if (is.character(checked_attempt)) return(checked_attempt)
-  
   test_rule(attempt)
 }
 
