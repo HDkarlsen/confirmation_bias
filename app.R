e7b10dc364ee0c548224df963dc3e828a36a28f0
hkarlsen <h.karlsen@ntnu.no>
2023-05-24 13:08:00 +0200

    Added submit rule button and feedback.

--- app.R
+++ app.R
@@ -22,14 +22,26 @@ ui <- fluidPage(
     )
   ),
   fluidRow(
-    column(width = 9,
+    column(width = 7,
            h2("Results"),
            textOutput("value")
-    )
+    ),
+    column(width = 3,
+           h2("Think you know the answer?"),
+           p("When you believe that you know the rule that the sequence follows, you may submit it here. 
+             Note that you can only do it ONCE, so make sure you're satisfied with you answer before attempting. 
+             You don't have to worry about exact phrasing."),
+           textInput("rule_answer", label = "What is the rule that these three numbers follow?"),
+           actionButton("rule_given", label = "Submit"),
+           textOutput("true_rule")
+           )
   )
 )
 
 server <- function(input, output) {
+  
+  # Initialises rule_out for later (might not be necessary).
+  rule_out <- ""
   
   # Reacts when button is pressed
   out <- eventReactive(input$do, {
@@ -40,6 +52,17 @@ server <- function(input, output) {
   # Sends results to the ui.
   output$value <- renderPrint({
     out()
+  })
+  
+  # Initiates rule explanation when button is pressed
+  rule_out <- eventReactive(input$rule_given, {
+    return('"Any numbers in ascending order"')
+  })
+  
+  # I'd like a linebreak. 
+  output$true_rule <- renderText({
+    paste0('You said: "', input$rule_answer,  '"--- \n The real rule is: ', rule_out(),
+          ". Did you get it right?")
   })
   
 }
